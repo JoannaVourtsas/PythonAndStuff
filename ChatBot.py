886f1566f0ba3913898c2634df88352868cb271d
JoannaVourtsas <109107503+JoannaVourtsas@users.noreply.github.com>
2022-07-25 13:43:12 -0700

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -1,20 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
 print('Welcome to created your own character \nMy name is Bot\nWhat is your name?') #Print welcome , Asks user for name
-
-
-# In[2]:
-
-
+Welcome to created your own character 
+My name is Bot
+What is your name?
 name = input()
-
-
-# In[3]:
-
-
+hello
 print('Hello, ', name)
-
