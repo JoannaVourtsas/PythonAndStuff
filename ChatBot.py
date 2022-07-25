8317bd5284d9694bb1a862f168bb8a1907376be8
JoannaVourtsas <109107503+JoannaVourtsas@users.noreply.github.com>
2022-07-25 13:34:32 -0700

    Create ChatBot.py

---
+++ ChatBot.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('Welcome to created your own character \nMy name is Bot\nWhat is your name?') #Print welcome , Asks user for name
+
+
+# In[2]:
+
+
+name = input()
+
+
+# In[3]:
+
+
+print('Hello, ', name)
+
